2b6a69a849ddc2fdcceb745c055f0a8ccad3a457
Carlo Serame <carloserame24@gmail.com>
2021-12-11 08:28:09 -0800

    CSS

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,7 @@
 import { useState } from "react";
-import ReactMapGL from "react-map-gl";
+import ReactMapGL, { Marker, Popup } from "react-map-gl";
+import { Room, Star } from "@material-ui/icons";
+import "./app.css"
 
 function App() {
   const [viewport, setViewport] = useState({
@@ -16,7 +18,43 @@ function App() {
         {...viewport}
         mapboxApiAccessToken={process.env.REACT_APP_MAPBOX}
         onViewportChange={(nextViewport) => setViewport(nextViewport)}
-      />
+        mapStyle = "mapbox://styles/safak/cknndpyfq268f17p53nmpwira"
+      >
+        <Marker
+          latitude={48.85093}
+          longitude={2.294694}
+          offsetLeft={-20}
+          offsetTop={-10}
+        >
+          <Room style ={{fontSize:viewport.zoom * 7, color:"slateblue"}}/>
+        </Marker>
+        <Popup
+          latitude={48.85093}
+          longitude={2.294694}
+          closeButton={true}
+          closeOnClick={false}
+          anchor="left">
+          <div className="card">
+            <label>Place</label>
+            <h4 className="place">Eiffel Tower</h4>
+            <label>Review</label>
+            <p className="desc">Beautiful place. I love it.</p>
+            <label>Rating</label>
+            <div classname="stars">
+            <Star className="star"/>
+            <Star className="star"/>
+            <Star className="star"/>
+            <Star className="star"/>
+            <Star className="star"/>
+           
+            </div>
+            <label>Information</label>
+            <span className="username">Created by <b>Madison</b></span>
+            <span className="date">1 hour ago</span>
+
+          </div>
+        </Popup>
+      </ReactMapGL>
     </div>
   );
 }
